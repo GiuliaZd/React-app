7745f09862ca156db279625e6b07b4afd5ede21f
GiuliaZd <yuliya.zd@gmail.com>
2023-11-28 16:41:53 +0200

    added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  return (
+//   return (
+//     <>
+//       {count <= 10 
+//         ? <p>You have pressed the button {count} times</p> 
+//         : <p>You have pressed the button more than 10 times</p>
+//       }
+//       <button onClick={() => setCount(count + 1)}>Add1</button>
+//       <button onClick={() => setCount(count -1)}>Minus1</button>
+//       <button onClick={() => setCount(0)}>Reset</button>
+//     </>
+//   );
+// }
+
+function App({message}) {
+  return(
     <>
-      {count <= 10 
-        ? <p>You have pressed the button {count} times</p> 
-        : <p>You have pressed the button more than 10 times</p>
-      }
-      <button onClick={() => setCount(count + 1)}>Add1</button>
-      <button onClick={() => setCount(count -1)}>Minus1</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </>
-  );
+  {(message.length >10) 
+    ? <p>Too long</p>
+    : <p>{message}</p>}
+  </>);
 }
 
 export default App;
